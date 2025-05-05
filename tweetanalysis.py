d14138190eeba908383bfb96f70c3e9adba3ac89
sarahaliene <57459131+sarahaliene@users.noreply.github.com>
2025-05-05 15:13:42 -0500

    Update tweetanalysis.py

--- tweetanalysis.py
+++ tweetanalysis.py
@@ -2,8 +2,8 @@ import os
 from naivebayestweets import naive_bayes_tweet
 from vocab import create_vocab
 from predicttweet import predict_tweet
-from vadersentiment import get_vader_sentiment
 from testing import test_predictions
+from vadersentiment import get_vader_sentiment
 
 #Run entire processing script
 with open("processing.py") as file:
@@ -16,11 +16,11 @@ neg_tweets = current_path + "/processed_data/neg_tweets.csv"
 test_tweets = current_path + "/processed_data/tweets.csv"
 
 #Initialize naive_bayes values
-naive_bayes_tweet(pos_tweets, neg_tweets, 1.0)
+naive_bayes_tweet(1.0)
 
 #Get top 200 words for pos and neg tweets
-top_200_pos = create_vocab(pos_tweets)
-top_200_neg = create_vocab(neg_tweets)
+top_200_pos = create_vocab('pos_tweets.pkl')
+top_200_neg = create_vocab('neg_tweets.pkl')
 
 #Print top 10 of each
 print("Top 10 Positive Words:", top_200_pos[:10])
@@ -28,10 +28,10 @@ print("Top 10 Negative Words:", top_200_neg[:10])
 
 #Print predictions for pre-labeled files
 print("Testing pre-labeled positive tweets using Naive Bayes")
-test_predictions(pos_tweets)
+test_predictions('pos_tweets.pkl')
 
 print("Testing pre-labeled negative tweets using Naive Bayes")
-test_predictions(neg_tweets)
+test_predictions('neg_tweets.pkl')
 
 print("Predictions for unlabeled test file tweets using Naive Bayes")
-test_predictions(test_tweets)
+test_predictions('tweets.pkl')
