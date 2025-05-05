6f789632eb52bc85daf52e8c7b6d6c1ee3907bff
sarahaliene <57459131+sarahaliene@users.noreply.github.com>
2025-05-05 15:14:32 -0500

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,33 +1,30 @@
 import os
 import pandas as pd
+import pickle
 from collections import Counter
 from predicttweet import predict_tweet
 from vadersentiment import get_vader_sentiment
-#Vader sentiment code commented out - doesn't seem to be picking up on tone well
 
 
 def test_predictions(tweets_file_name):
-    tweets = pd.read_csv(tweets_file_name)
 
-    tweets_list = []
+    tweets_file = open(tweets_file_name, 'rb')    
+    tweets_list = pickle.load(tweets_file)
+
+
     prediction_list = []
-    #vader_list = []
+    vader_list = []
     prediction_counter = Counter()
-
-    #Add each line (tweet) to list
-    for index, row in tweets.iloc[1:].iterrows():
-
-        tweets_list.append(row)
 
     #Get prediction for each tweet in list
     for tweet in tweets_list:
 
         pred = predict_tweet(tweet)
-        #vader_score = get_vader_sentiment(tweet)
+        vader_score = get_vader_sentiment(tweet)
         prediction_list.append(pred)
-        #vader_list.append(vader_score)
+        vader_list.append(vader_score)
 
     prediction_counter.update(prediction_list)
     print("Positive:", prediction_counter['positive'])
     print("Negative:", prediction_counter['negative'])
-    #print(prediction_list[:50], vader_list[:50])
+    print(prediction_list[:50], vader_list[:50])
