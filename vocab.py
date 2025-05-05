6130ae2486c9da58d195b095108ddbf245a088fb
sarahaliene <57459131+sarahaliene@users.noreply.github.com>
2025-05-05 15:12:02 -0500

    Update vocab.py

--- vocab.py
+++ vocab.py
@@ -1,19 +1,14 @@
 from collections import Counter
 import pandas as pd
 import os
-#Functioning as of 5-5-25
+import pickle
 
 def create_vocab(tweet_file_name):
 
     tweet_counter = Counter()
 
-    tweets = pd.read_csv(tweet_file_name)
-    tweets_list = []
-
-    #Add each line (tweet) to the counter
-    for index, row in tweets.iloc[1:].iterrows():
-
-        tweets_list.append(row)
+    tweets_file = open(tweet_file_name, 'rb')    
+    tweets_list = pickle.load(tweets_file)
 
     for tweet in tweets_list:
 
