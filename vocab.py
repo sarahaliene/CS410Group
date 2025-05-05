bf3c85aa819cf2ec79290d48c5daf3d5056a6acb
sarahaliene <57459131+sarahaliene@users.noreply.github.com>
2025-05-05 09:50:48 -0500

    Update vocab.py

--- vocab.py
+++ vocab.py
@@ -1,15 +1,26 @@
 from collections import Counter
+import pandas as pd
+import os
+#Functioning as of 5-5-25
 
-def create_vocab(tweet_list, n):
+def create_vocab(tweet_file_name):
 
-	tweet_counter = Counter()
+    tweet_counter = Counter()
 
-	#Add each line (tweet) to the counter
-	for tweet in tweet_list:
-		tweet_counter.update(tweet)
+    tweets = pd.read_csv(tweet_file_name)
+    tweets_list = []
+
+    #Add each line (tweet) to the counter
+    for index, row in tweets.iloc[1:].iterrows():
+
+        tweets_list.append(row)
+
+    for tweet in tweets_list:
+
+        tweet_counter.update(tweet)
 
 
-	top_200 = tweet_counter.most_common(n)
+    top_200 = tweet_counter.most_common(200)
 
-
-	return top_200
+    print("Finished vocab creation")
+    return top_200
