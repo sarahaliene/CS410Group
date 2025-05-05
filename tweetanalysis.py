16c4ac8b26d5904911f568287f5ad5fabc98c75a
sarahaliene <57459131+sarahaliene@users.noreply.github.com>
2025-05-04 21:29:58 -0500

    Update tweetanalysis.py

--- tweetanalysis.py
+++ tweetanalysis.py
@@ -1,43 +1,14 @@
-#CS410 Rough code tweet analysis
+import os
+from naivebayestweets import naive_bayes_tweet
 
-import numpy as np
-import nltk
+#Run entire processing script
+with open("processing.py") as file:
+    exec(file.read())
 
-#Stopwords
-from nltk.corpus import stopwords as stopwords
+current_path = os.path.dirname(__file__)
 
-#Tweet Tokenizer
-#from https://www.nltk.org/api/nltk.tokenize.casual.html#module-nltk.tokenize.casual
-from nltk.tokenize import TweetTokenizer
+pos_tweets = current_path + "/processed_data/pos_tweets.csv"
+neg_tweets = current_path + "/processed_data/neg_tweets.csv"
+test_tweets = current_path + "/processed_data/tweets.csv"
 
-#Word stemmer
-from nltk.stem.snowball import EnglishStemmer
-
-#Words considered valid words by nltk, may take too much time to run
-#from nltk.corpus import words as nltkwords
-
-#NLTK Sentiment Analyzer tool
-from nltk.sentiment import sentiment_analyzer
-
-#NLTK basic sentiment analysis utilities
-#demo_liu_hu_lexicon(sentence, plot=False) -> uses Liu Hu lexicon for pos/neg/neu word classification
-#demo_sent_subjectivity(text) -> classify sentence as subjective or objective
-from nltk.sentiment import util as sentiment_util
-
-#For counting occurences of things in general
-from collections import Counter
-
-#These may need to be downloaded?
-nltk.download('punkt_tab')
-nltk.download('stopwords')
-
-#Use stemmer.stem(word) to stem word
-stemmer = EnglishStemmer()
-
-#Twitter specific imports
-#Twitter corpus from NLTK
-#twitter_samples includes 3 files for negative, positive, and tweets
-#.strings accesses text fields
-from nltk.corpus import twitter_samples
-nltk.download("twitter_samples")
-from nltk.twitter import json2csv
+naive_bayes_tweet(pos_tweets, neg_tweets, 1.0)
